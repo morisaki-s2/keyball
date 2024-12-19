1877130fddac74e7cdf8ed571b70eb24aa683b45
morisaki <morisaki@s2factory.co.jp>
2024-12-19 16:30:25 +0900

    config.h 修正

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/morisaki/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/morisaki/config.h
@@ -35,9 +35,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAP_CODE_DELAY 5
 
-#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
-#define AUTO_MOUSE_DEFAULT_LAYER 1
-
 // キー長押し時間
 #define TAPPING_TERM 250
 
